385136aa21ad2cb1dc7f9069e35b76d9025b508b
unknown <harshal.kumeriya183@gmail.com>
2020-01-27 23:40:02 +0530

    new library and dataset

---
+++ linear_regression.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+import sklearn.preprocessing import train_test_split
+import sklearn.linear_model import LinearRegression
+import sklearn.preprocessing import StandardScaler
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df = pd.read_csv('H://Datasets//.csv')
+print(df)
